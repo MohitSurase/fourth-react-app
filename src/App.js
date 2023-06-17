d15b6b7940a56ba91100e114c71d0a96546b6bef
Mohit Surase <surasemohit7@gmail.com>
2023-06-17 17:21:26 +0530

    Navigation Menu in Router tag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import MyTodo from "./Pages/MyTodo";
 import MyRegistration from "./Pages/MyRegistration";
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Link to={"/home"}>Home |</Link>
+        <Link to={"/todo"}>Todo |</Link>
+        <Link to={"/registration"}>Registration </Link>
+
         <Routes>
           <Route path="/" element={<h1>Hello </h1>} />
           <Route path="/home" element={<h1>Home </h1>} />
