34c9f9b5304486681a2afd71771b5ee9cabfd348
Mohit Surase <surasemohit7@gmail.com>
2023-06-17 09:58:28 +0530

    Register Trial

--- src/App.js
+++ src/App.js
@@ -3,74 +3,51 @@ import { useRef, useState } from "react";
 function App() {
   return (
     <>
-      <h1>Counter App</h1>
-      <CounterApp />
-      <h1>Stateful List</h1>
-      <MyTodo />
+      <h1>My project</h1>
+      <ListDemo />
     </>
   );
 }
 
-function CounterApp() {
-  let [Counter, setCounter] = useState(1);
-  let [Counter2, setCounter2] = useState(1);
-  let LikeMeAction = () => {
-    Counter = Counter + 1;
-    setCounter(Counter);
-  };
-
-  let DislikeAction = () => {
-    Counter2 = Counter2 + 1;
-    setCounter2(Counter2);
-  };
-  return (
-    <>
-      {Counter}
-      <input type="button" value="LikeMe" onClick={LikeMeAction} />
-      {Counter2}
-      <input type="button" value="Dislike" onClick={DislikeAction} />
-    </>
-  );
-}
-
-function MyTodo() {
+function ListDemo() {
   let inputRef = useRef();
-  let [Todo, SetTodo] = useState(["Hi"]);
+  let [List, SetList] = useState(["Hi"]);
 
   let AddMessage = () => {
     let inputValue = inputRef.current.value;
-    let newTodo = [...Todo, inputValue];
-    SetTodo(newTodo);
+    let newList = [...List, inputValue];
+    SetList(newList);
 
     inputRef.current.value = "";
   };
   return (
     <>
-      <input type="text" ref={inputRef} placeholder="Enter Message" />
-      <input type="button" value="Add Message" onClick={AddMessage} />
-
-      {Todo.map((item) => (
-        <Context message={item} />
-      ))}
-    </>
-  );
-}
-
-function Context({ message }) {
-  return (
-    <>
-      <h1>Context {message}</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, ab
-        officia eveniet nihil eligendi maxime. Fugiat quaerat ut sed earum
-        consequuntur voluptatibus praesentium at, sint officia, dolor impedit
-        magnam libero? Minima quo accusantium maiores a ea maxime tempore,
-        corrupti, in accusamus repellat, voluptatibus nostrum commodi debitis
-        dolorem libero illum. Quo?
-      </p>
-      <div>
-        <input type="button" value="&#128077;" />
-        <input type="button" value="&#128078;" />
+      <div
+        className="row justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
+        <div className="col-sm-12 col-md-6">
+          <h1>Register App</h1>
+          <input
+            type="text"
+            className="form-control"
+            ref={inputRef}
+            placeholder="Enter User Input"
+          />
+          <input
+            type="text"
+            className="form-control"
+            ref={inputRef}
+            placeholder="Password"
+          />
+          <input
+            type="text"
+            className="form-control"
+            ref={inputRef}
+            placeholder="Email"
+          />
+          <input type="button" value="Login" onClick={AddMessage} />
+        </div>
       </div>
     </>
   );
