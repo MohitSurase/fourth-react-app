00c1d43995f32b8d6502b20bc2da283990899e74
Mohit Surase <surasemohit7@gmail.com>
2023-06-17 18:29:18 +0530

    Todo-list-retreival from DB

--- src/Pages/MyTodoList.js
+++ src/Pages/MyTodoList.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 function MyTodoList() {
+  let [todoList, setTodoList] = useState([]);
+
+  let getAllTodoAction = async () => {
+    let url = "http://localhost:4000/find-all-todo";
+
+    let res = await fetch(url);
+    let list = await res.json();
+    console.log(list);
+
+    setTodoList([...list]);
+  };
   return (
     <>
       <h1>Display all Todo</h1>
+      <input
+        type="button"
+        value="Get all Data from Server"
+        onClick={getAllTodoAction}
+      />
+
+      {todoList.map((item) => (
+        <>
+          <div className="alert alert-success">
+            {item.task}
+            <p>{item.description}</p>
+          </div>
+        </>
+      ))}
     </>
   );
 }
