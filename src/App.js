d675ccbaf5f4055ef46354c57a9bd625a042e07e
Mohit Surase <surasemohit7@gmail.com>
2023-06-17 18:06:55 +0530

    Todo List function

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import MyTodo from "./Pages/MyTodo";
 import MyRegistration from "./Pages/MyRegistration";
 import MyNavigationLinks from "./Pages/MyNavigationLinks";
 import MyNavigationLinksUsingButton from "./Pages/MyNavigationLinksUsingButton";
+import MyTodoList from "./Pages/MyTodoList";
 function App() {
   return (
     <>
@@ -18,10 +19,10 @@ function App() {
           <Route path="/" element={<h1>Hello </h1>} />
           <Route path="/home" element={<h1>Home </h1>} />
           <Route path="/todo" element={<MyTodo />} />
+          <Route path="/todo-list" element={<MyTodoList />} />
           <Route path="/registration" element={<MyRegistration />} />
           <Route path="*" element={<h1>Page not found </h1>} />
         </Routes>
-        <MyNavigationLinksUsingButton />
       </BrowserRouter>
     </>
   );
---
+++ src/Pages/MyTodoList.js
@@ -0,0 +1,9 @@
+function MyTodoList() {
+  return (
+    <>
+      <h1>Display all Todo</h1>
+    </>
+  );
+}
+
+export default MyTodoList;
