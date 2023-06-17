c81e96b1ee06fad75d3a169cd011f7b81552f32b
Mohit Surase <surasemohit7@gmail.com>
2023-06-17 17:11:57 +0530

    Basic Setup

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           <Route path="/home" element={<h1>Home </h1>} />
           <Route path="/todo" element={<MyTodo />} />
           <Route path="/registration" element={<MyRegistration />} />
+          <Route path="*" element={<h1>Page not found </h1>} />
         </Routes>
       </BrowserRouter>
     </>
